d06b57f82f58ecd0dec760558b2dfc1a514e5ff4
vincegu <vince.gu@outlook.com>
2021-08-15 01:11:31 +1000

    dataEntry class created

--- src/main/java/dataEntry.java
+++ src/main/java/dataEntry.java
@@ -1,3 +1,28 @@
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Document(collection="revisions")
+@AllArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class dataEntry {
+    @Id
+    private String id;
+    private int revid;
+    private int parentid;
+    private boolean minor;
+    private String user;
+    private int userid;
+    private Date timestamp;
+    private int size;
+    private String sha1;
+    private String parsedcomment;
+    private String title;
 }
